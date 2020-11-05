f913c23bb04966b6a5bec3c429fe59f47f610a8a
Torix05 <taejun06152gmail.com>
2020-11-05 22:33:51 +0900

    setting
    초기 환경 셋팅

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { ReactComponent } from "*.svg";
+import "./App.css";
+export default class App extends React.Component {
+   render() {
+      return (
+         <div className="App">
+            <header className="App-header">
+               <h1>TEST!!!!!!!!!!!!</h1>
+            </header>
+         </div>
+      );
+   }
+}
+export default App;
